bce853fd6096a92e5e661c2e4873fc87f1776658
rvost <romanvost@gmail.com>
2023-11-23 02:11:51 +0300

    feat: Improve diagnostics for spawnable types
    Diagnose attributes and configuration when preset is specified.

--- lemminx-dayz-ce/src/main/java/io/github/rvost/lemminx/dayz/participants/diagnostics/SpawnableTypesDiagnosticsParticipant.java
+++ lemminx-dayz-ce/src/main/java/io/github/rvost/lemminx/dayz/participants/diagnostics/SpawnableTypesDiagnosticsParticipant.java
@@ -17,6 +17,10 @@ import static io.github.rvost.lemminx.dayz.participants.diagnostics.DiagnosticsU
 public class SpawnableTypesDiagnosticsParticipant implements IDiagnosticsParticipant {
     private static final String INVALID_RANDOM_PRESET_CODE = "invalid_random_preset";
     private static final String INVALID_RANDOM_PRESET_MESSAGE = "%s preset \"%s\" does not exist.";
+    private static final String CONFIGURATION_NOT_ALLOWED_CODE = "configuration_not_allowed";
+    private static final String CONFIGURATION_NOT_ALLOWED_MESSAGE = "Configuration not allowed when preset is specified.";
+    private static final String ATTRIBUTE_NOT_ALLOWED_CODE = "attribute_not_allowed";
+    private static final String ATTRIBUTE_NOT_ALLOWED_MESSAGE = "Attribute not allowed when preset is specified.";
 
     private final DayzMissionService missionService;
 
@@ -37,14 +41,27 @@ public class SpawnableTypesDiagnosticsParticipant implements IDiagnosticsPartici
             if (typeNode.hasChildNodes()) {
                 for (var node : typeNode.getChildren()) {
                     var kind = node.getNodeName();
-                    if (node.hasAttribute(SpawnableTypesModel.PRESET_ATTRIBUTE) && randomPresets.containsKey(kind)) {
-                        var attr = node.getAttributeNode(SpawnableTypesModel.PRESET_ATTRIBUTE);
-                        if (!randomPresets.get(kind).contains(attr.getValue())) {
-                            var attrValue = attr.getNodeAttrValue();
-                            var range = XMLPositionUtility.createRange(attrValue);
-                            String message = String.format(INVALID_RANDOM_PRESET_MESSAGE, kind, attr.getValue());
-                            diagnostics.add(new Diagnostic(range, message, DiagnosticSeverity.Error, ERROR_SOURCE, INVALID_RANDOM_PRESET_CODE));
+                    if (node.hasAttribute(SpawnableTypesModel.PRESET_ATTRIBUTE)) {
+                        if (randomPresets.containsKey(kind)) {
+                            var attr = node.getAttributeNode(SpawnableTypesModel.PRESET_ATTRIBUTE);
+                            if (!randomPresets.get(kind).contains(attr.getValue())) {
+                                var attrValue = attr.getNodeAttrValue();
+                                var range = XMLPositionUtility.createRange(attrValue);
+                                String message = String.format(INVALID_RANDOM_PRESET_MESSAGE, kind, attr.getValue());
+                                diagnostics.add(new Diagnostic(range, message, DiagnosticSeverity.Error, ERROR_SOURCE, INVALID_RANDOM_PRESET_CODE));
+                            }
                         }
+                        if (node.hasChildNodes()) {
+                            var range = XMLPositionUtility.createRange(node.getStart(), node.getEnd(), node.getOwnerDocument());
+                            diagnostics.add(new Diagnostic(range, CONFIGURATION_NOT_ALLOWED_MESSAGE, DiagnosticSeverity.Error, ERROR_SOURCE, CONFIGURATION_NOT_ALLOWED_CODE));
+                        }
+                        node.getAttributeNodes().stream()
+                                .filter(n -> !SpawnableTypesModel.PRESET_ATTRIBUTE.equals(n.getName()))
+                                .map(n -> {
+                                    var range = XMLPositionUtility.createRange(n);
+                                    return new Diagnostic(range, ATTRIBUTE_NOT_ALLOWED_MESSAGE, DiagnosticSeverity.Error, ERROR_SOURCE, ATTRIBUTE_NOT_ALLOWED_CODE);
+                                })
+                                .forEach(diagnostics::add);
                     }
                 }
             }
